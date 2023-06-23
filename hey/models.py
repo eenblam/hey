08b8e7bd6ca2eaba33f69735dc8707469c116d43
eenblam <eenblam@riseup.net>
2023-06-23 10:08:20 -0700

    Improve Friend model
    * DateField not DateTimeField
    * Not all fields required
    * __str__

--- hey/models.py
+++ hey/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Friend(models.Model):
-    first_name = models.TextField()
-    last_name = models.TextField()
-    birthday = models.DateTimeField("birthday")
+    first_name = models.TextField() # required
+    last_name = models.TextField(blank=True)
+    #TODO timezones?
+    birthday = models.DateField("birthday", blank=True, null=True)
     #TODO don't want to restrict by char count for internationalization
-    phone = models.TextField()
+    phone = models.TextField(blank=True)
+
+    def __str__(self):
+        if self.last_name != "":
+            return f'{self.first_name} {self.last_name}'
+        return self.first_name
